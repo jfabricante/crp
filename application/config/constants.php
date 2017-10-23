68774ebf04df2a2e8e34a95244dcefa574fb7bdb
Jerome <jerome-fabricante@isuzuphil.com>
2017-10-23 10:47:55 +0800

    Define constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,12 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+// Define time attribute constant
+defined('YEAR')   OR define('YEAR', 31536000);
+defined('MONTH')  OR define('MONTH', 2592000);
+defined('WEEK')   OR define('WEEK', 604800);
+defined('DAY')    OR define('DAY', 86400);
+defined('HOUR')   OR define('HOUR', 3600);
+defined('MINUTE') OR define('MINUTE', 60);
+
