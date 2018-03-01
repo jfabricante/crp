122358f11baa9898bb628bd6177fe224d3af11bc
Unknown <jerome-fabricante@isuzuphil.com>
2018-03-01 10:38:23 +0800

    Refactor download doc

--- application/controllers/Docs.php
+++ application/controllers/Docs.php
@@ -241,28 +241,12 @@ class Docs extends CI_Controller
 
 	public function download_doc()
 	{
-		$path = base_url('resources/docs/' . $this->uri->segment(3));
+		$name = $this->uri->segment(3);
+		$path = base_url('resources/docs/' . $name);
 
-		// required for IE
-		if(ini_get('zlib.output_compression')) { ini_set('zlib.output_compression', 'Off'); }
-
-		// get the file mime type using the file extension
-		$this->load->helper('file');
-
-		$mime = get_mime_by_extension($path);
-
-		// Build the headers to push out the file properly.
-		header('Pragma: public');     // required
-		header('Expires: 0');         // no cache
-		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-		header('Last-Modified: '.gmdate ('D, d M Y H:i:s', filemtime ($path)).' GMT');
-		header('Cache-Control: private',false);
-		header('Content-Type: '.$mime);  // Add the mime type from Code igniter.
-		header('Content-Disposition: attachment; filename="'.basename($name).'"');  // Add the file name
-		header('Content-Transfer-Encoding: binary');
-		header('Content-Length: '.filesize($path)); // provide file size
-		header('Connection: close');
-		readfile($path); // push it out
+		header("Content-type: application/octet-stream");
+		header('Content-Disposition: attachment; filename="' . $name . '"');
+		readfile($path);
 		exit();
 	}
 
