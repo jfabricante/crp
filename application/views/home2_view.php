3480c8b37d8a984fee9e4f5e29391d9261ded5c2
Unknown <jerome-fabricante@isuzuphil.com>
2018-05-17 09:21:00 +0800

    Change images
    Wrap with anchor tags. Change the cursor to pointer when hovering.

--- application/views/home2_view.php
+++ application/views/home2_view.php
@@ -4,6 +4,10 @@
 	}
 
 	.gallery__item {
+		cursor: pointer;
+	}
+
+	.gallery__footer {
 		padding: 15px;
 		background: #d72b3b;
 		color: #fff;
@@ -17,31 +21,67 @@
 		<h1 class="text-danger">DISCOVER</h1>
 
 		<div class="gallery">
-			<div class="col-md-4">
-				<img class="img-responsive" src="<?php echo base_url('resources/images/ceseries-new.jpg') ?>" />
-				<div class="text-left text-danger gallery__item">
-					<h3 class="gallery-header">N-SERIES</h3>
-					<p class="gallery-desc">Light-Duty Trucks</p>
+			<a href="<?php echo base_url('index.php/NSERIES/N_series'); ?>" target="_blank">
+				<div class="col-md-3 gallery__item">
+					<img class="img-responsive" src="<?php echo base_url('resources/images/gr1_9680-qkr-truck-quarter-left-crop.jpg') ?>" />
+					<div class="text-left text-danger gallery__footer">
+						<h4 class="gallery-header">N-SERIES</h4>
+						<p class="gallery-desc">Light-Duty Trucks</p>
+					</div>
 				</div>
-			</div>
-			<div class="col-md-4">
-				<img class="img-responsive" src="<?php echo base_url('resources/images/f-series-updated.jpg') ?>" />
-				<div class="text-left text-danger gallery__item">
-					<h3 class="gallery-header">F-SERIES</h3>
-					<p class="gallery-desc">Medium-Duty Trucks</p>
+			</a>
+
+			<a href="<?php echo base_url('index.php/FSERIES/F_series');?>" target="_blank">
+				<div class="col-md-3 gallery__item">
+					<img class="img-responsive" src="<?php echo base_url('resources/images/frr90-live-crop.jpg') ?>" />
+					<div class="text-left text-danger gallery__footer">
+						<h4 class="gallery-header">F-SERIES</h4>
+						<p class="gallery-desc">Medium-Duty Trucks</p>
+					</div>
 				</div>
-			</div>
-			<div class="col-md-4">
-				<img class="img-responsive" src="<?php echo base_url('resources/images/nseries-new.jpg') ?>" />
-				<div class="text-left text-danger gallery__item">
-					<h3 class="gallery-header">C&E Series</h3>
-					<p class="gallery-desc">Heavy-Duty Trucks</p>
+			</a>
+
+			<a href="<?php echo base_url('index.php/VESI/c_series');?>" target="_blank">
+				<div class="col-md-3 gallery__item">
+					<img class="img-responsive" src="<?php echo base_url('resources/images/ceseries-arcwhite-cyz-3-crop.jpg') ?>" />
+					<div class="text-left text-danger gallery__footer">
+						<h4 class="gallery-header">C&E Series</h4>
+						<p class="gallery-desc">Heavy-Duty Trucks</p>
+					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 		<!-- End of col-md-6 -->
 	</div>
 	<!-- End of row -->
+	
+	<!-- Spacing between row -->
+	<br>
+
+	<!-- row -->
+	<div class="row text-center">
+		<a href="<?php echo base_url('index.php/USERIES/U_series') ?>" target="_blank">
+			<div class="col-md-3 gallery__item">
+				<img class="img-responsive" src="<?php echo base_url('resources/images/mu-x-red-spinel-crop.jpg') ?>" />
+				<div class="text-left text-danger gallery__footer">
+					<h4 class="gallery-header">mu-X</h4>
+					<p class="gallery-desc">Center of Attraction</p>
+				</div>
+			</div>
+		</a>
+
+		<a href="<?php echo base_url('index.php/TSERIES/T_series'); ?>" target="_blank">
+			<div class="col-md-3 gallery__item">
+				<img class="img-responsive" src="<?php echo base_url('resources/images/dmax-xseries-white-crop.jpg') ?>" />
+				<div class="text-left text-danger gallery__footer">
+					<h4 class="gallery-header">D-MAX</h4>
+					<p class="gallery-desc">Tough Enough for Anything</p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- End of row -->
+
 </section>
 <div class="modal fade bs-example-modal-sm" role="dialog" aria-labelledby="mySmallModalLabel">
   <div class="modal-dialog modal-sm" role="document">
